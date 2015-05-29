ceb3a8ba4741f202cec64153412d23470c5c01d7
Jaime Gabriel Jingco <gabatrone11@gmail.com>
2015-05-28 18:19:34 -0700

    implemented devise for users

--- Gemfile
+++ Gemfile
@@ -1,9 +1,8 @@
 source 'https://rubygems.org'
-ruby '2.0.0'
+ruby '2.1.5'
 
 
 gem 'rails', '4.1.8'
-
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
@@ -11,6 +10,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass', '~> 3.3.4.1'
+gem 'devise', '~> 3.5.1'
 
 group :development, :test do
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,9 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  #Required for Heroku
+  config.action_mailer.default_url_options = { :host => 'https://gabs-omr-pinteresting.herokuapp.com/' }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
